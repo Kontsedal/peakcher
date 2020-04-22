3d3fb1fcc242016b1ee51fb411308db1f33e883d
Kontsedal <kontsedalbohdan@gmail.com>
2020-04-22 22:35:07 +0300

    add background and content script entry points

--- webpack/common.js
+++ webpack/common.js
@@ -1,24 +1,21 @@
 const path = require("path");
-const PATHS = require('./paths')
-const loaders = require("./loaders")
-const plugins = require("./plugins")
+const PATHS = require("./paths");
+const loaders = require("./loaders");
+const plugins = require("./plugins");
 
 const ENTRY_POINTS = {
   popup: path.join(PATHS.SRC_DIR, "popup/index.tsx"),
+  background: path.join(PATHS.SRC_DIR, "background/index.ts"),
+  contentScript: path.join(PATHS.SRC_DIR, "contentScript/index.ts"),
 };
 
 module.exports = {
   entry: ENTRY_POINTS,
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: [".tsx", ".ts", ".js"],
   },
   module: {
-    rules: [
-      loaders.processTypescript,
-      loaders.processSass
-    ],
+    rules: [loaders.processTypescript, loaders.processSass],
   },
-  plugins: [
-    plugins.processPopupHtmlFile
-  ]
+  plugins: [plugins.processPopupHtmlFile],
 };
