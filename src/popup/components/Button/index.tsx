f57946e874c4dc59773de7c34c8f808d7e2dbf59
Kontsedal <kontsedalbohdan@gmail.com>
2020-08-20 19:59:50 +0300

    Fix linting issues for Button component

--- src/popup/components/Button/index.tsx
+++ src/popup/components/Button/index.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import cn from "classnames";
+import PropTypes from "prop-types";
 import styles from "./styles.module.scss";
 
 type Props = {
-  children?: any;
+  children?: React.ReactNode;
   className?: string;
   primary?: boolean;
   flat?: boolean;
   onClick?: () => void;
-  [key: string]: any;
 };
-export const Button = ({
+export const Button: React.FC<Props> = ({
   children,
   className,
   primary,
   flat,
   onClick,
-  ...rest
-}: Props) => (
+}) => (
   <button
+    type="button"
     className={cn(
       styles.button,
       primary && styles.primary,
@@ -26,8 +26,23 @@ export const Button = ({
       className
     )}
     onClick={onClick}
-    {...rest}
   >
     {children}
   </button>
 );
+
+Button.defaultProps = {
+  children: null,
+  className: null,
+  primary: false,
+  flat: false,
+  onClick: null,
+};
+
+Button.propTypes = {
+  children: PropTypes.element,
+  className: PropTypes.string,
+  primary: PropTypes.bool,
+  flat: PropTypes.bool,
+  onClick: PropTypes.func,
+};
