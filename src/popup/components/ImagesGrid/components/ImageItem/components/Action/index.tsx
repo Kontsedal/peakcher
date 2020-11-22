1f6a4fe7081c1a35049d2429835b098ee794a82d
Kontsedal <kontsedalbohdan@gmail.com>
2020-11-22 11:24:16 +0200

    Fix a11i issues

--- src/popup/components/ImagesGrid/components/ImageItem/components/Action/index.tsx
+++ src/popup/components/ImagesGrid/components/ImageItem/components/Action/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import cn from "classnames";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import styles from "../../styles.module.scss";
+import { handleEnter } from "../../../../../../../utils/keyHandler";
 
 export const Action: React.FC<{
   text: string;
@@ -11,14 +12,16 @@ export const Action: React.FC<{
   onCopy?: () => void;
 }> = ({ text, isLoading, onClick, textToCopy, onCopy }) => {
   const element = (
-    <button
-      type="button"
+    <div
+      tabIndex={0}
+      role="menuitem"
       onClick={!isLoading ? onClick : undefined}
+      onKeyPress={handleEnter(() => (!isLoading ? onClick : undefined))}
       className={cn(styles.moreActionsItem, isLoading && styles.loading)}
     >
       {isLoading && <div className={styles.loader} />}
       {!isLoading && <div className={styles.moreActionsItemText}>{text}</div>}
-    </button>
+    </div>
   );
   if (textToCopy) {
     return (
