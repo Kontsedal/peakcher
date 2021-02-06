e9da29156dbbe02c4eac105da815aced3a155f14
Kontsedal <kontsedalbohdan@gmail.com>
2021-02-06 12:30:59 +0200

    feat(painter): reduce memory consumption

--- src/popup/pages/EditImage/Painter/index.ts
+++ src/popup/pages/EditImage/Painter/index.ts
@@ -5,33 +5,7 @@ import { createShape } from "./shapes/factory";
 import { Drawable } from "./interfaces/drawable";
 import { ToolParams } from "./interfaces/toolParams";
 
-type CreateAction = {
-  type: "create";
-  payload: {
-    position: Position;
-    params: ToolParams;
-    shape: SHAPES;
-    shapeId: string;
-  };
-};
-type UpdateAction = {
-  type: "update";
-  payload: {
-    position: Position;
-    params: ToolParams;
-    shapeId: string;
-  };
-};
-type FinishAction = {
-  type: "finish";
-  payload: {
-    shapeId: string;
-  };
-};
-type Action = CreateAction | UpdateAction | FinishAction;
-
 export class Painter {
-  history: Action[] = [];
   currentShapeId: string;
   private shapesMap: Map<string, Drawable> = new Map<string, Drawable>();
   private shapesSet: Set<Drawable> = new Set<Drawable>();
@@ -45,53 +19,29 @@ export class Painter {
     params?: ToolParams;
   }) {
     this.currentShapeId = uuid();
-    this.history.push({
-      payload: { position, params, shape, shapeId: this.currentShapeId },
-      type: "create",
+    const shapeInstance = createShape({
+      position: position,
+      params: params,
+      shape: shape,
     });
-    this.updateShapes();
+    this.shapesSet.add(shapeInstance);
+    this.shapesMap.set(this.currentShapeId, shapeInstance);
   }
   update({ position, params }: { position?: Position; params?: ToolParams }) {
     if (!this.currentShapeId) {
       return;
     }
-    this.history.push({
-      payload: { position, params, shapeId: this.currentShapeId },
-      type: "update",
+    const shape = this.shapesMap.get(this.currentShapeId);
+    shape.update({
+      position: position,
+      params: params,
     });
-    this.updateShapes();
   }
   finish() {
     const currentShape = this.getCurrentShape();
-    debugger
     if (currentShape.type === SHAPES.BRUSH) {
       this.currentShapeId = undefined;
-      this.history.push({
-        payload: { shapeId: this.currentShapeId },
-        type: "finish",
-      });
     }
-  }
-  updateShapes() {
-    this.history.forEach((action) => {
-      if (action.type === "create") {
-        const shape = createShape({
-          position: action.payload.position,
-          params: action.payload.params,
-          shape: action.payload.shape,
-        });
-        this.shapesSet.add(shape);
-        this.shapesMap.set(action.payload.shapeId, shape);
-      }
-
-      if (action.type === "update") {
-        const shape = this.shapesMap.get(action.payload.shapeId);
-        shape.update({
-          position: action.payload.position,
-          params: action.payload.params,
-        });
-      }
-    });
   }
   getCurrentShape() {
     return this.shapesMap.get(this.currentShapeId);
